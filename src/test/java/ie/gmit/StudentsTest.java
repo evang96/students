a88e6a6e86440b3e1f15b61a74bfdc6052df8928
Evan Garvey <g00322695@gmit.ie>
2020-03-07 14:00:08 +0000

    First Commit again

--- src/test/java/ie/gmit/StudentsTest.java
+++ src/test/java/ie/gmit/StudentsTest.java
@@ -1,4 +1,13 @@
 package ie.gmit;
 
+import org.junit.Test;
+
 public class StudentsTest {
+
+    @Test
+    void testConstructor()
+    {
+        Students myStudents = new Students("Evan", "evangarvey96@hotmail.com");
+    }
+
 }
